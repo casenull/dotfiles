f6ad9ff291bf463868f15b9292b97f758e4e7f53
casenull <casenull@pm.me>
2023-03-02 13:13:55 +0100

    feat(nvim): Add 'Ansible'-command and keymaps

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -9,3 +9,29 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
 	end,
 	group = ftset,
 })
+
+local yaml_means_ansible = false
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	pattern = { "*.yaml", "*.yml" },
+	callback = function()
+		if yaml_means_ansible then
+			vim.opt_local.filetype = "yaml.ansible"
+		else
+			vim.opt_local.filetype = "yaml"
+		end
+	end,
+	group = ftset,
+})
+
+vim.api.nvim_create_user_command("Ansible", function()
+	yaml_means_ansible = not yaml_means_ansible
+	vim.notify("Ansible mode: " .. (yaml_means_ansible and "ON" or "OFF"))
+	if yaml_means_ansible and vim.opt_local.filetype == "yaml" then
+		vim.opt_local.filetype = "yaml.ansible"
+	else
+		if vim.opt_local.filetype == "yaml.ansible" then
+			vim.opt_local.filetype = "yaml"
+		end
+	end
+end, {})
