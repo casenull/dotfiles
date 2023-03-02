b3162f4135c967ce8bb1cb0d5e7105701339f998
casenull <casenull@pm.me>
2023-03-02 11:40:32 +0100

    fix(nvim): Various completion fixes
    - Words with umlauts are also accepted
    - Tab can be used to jump in snippets

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -14,23 +14,51 @@ return {
 
 		opts = function()
 			local cmp = require("cmp")
+			local luasnip = require("luasnip")
 
 			return { -- Table passed to require("cmp").setup()
 				preselect = cmp.PreselectMode.None,
 				snippet = {
 					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
+						luasnip.lsp_expand(args.body)
 					end,
 				},
 				mapping = cmp.mapping.preset.insert({
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.expandable() then
+							luasnip.expand()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if luasnip.expand_or_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
-					{ name = "buffer" },
+					{
+						name = "buffer",
+						option = {
+							keyword_pattern = [[\k\+]], -- use 'iskeyword', good for umlauts
+						},
+					},
 					{ name = "path" },
 					{ name = "nvim_lua" },
 				}),
